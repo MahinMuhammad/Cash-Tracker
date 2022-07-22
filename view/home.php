deca80b350ff1cff4ace2d36f78cd6fc29d52c28
MahinMuhammad <amimahinurrahman@gmail.com>
2022-07-22 16:35:39 +0600

    new way to display accounts on home

--- view/home.php
+++ view/home.php
@@ -22,7 +22,7 @@
             <legend>HOME</legend>
             <table align="center" width="100%">
                 <tr align="center">
-                    <td><a href='home.php'> CALCULATOR </a></td>
+                    <td width="300"><a href='home.php'> ADD ACCOUNT </a></td>
                     <td><a href='Settings.php'> SETTINGS </a></td>
                     <td><a href='home.php'> ABOUT US </a></td>
                     <td><a href='../control/logout.php'> LOGOUT </a></td>
@@ -30,21 +30,34 @@
             </table>
             <h1>Wellcome <?php require_once "../model/userModel.php"; echo getRealName($_COOKIE['status']);?> </h1>
             <h2>Account List</h2>
-            <table border="1" align="center">
-                <?php
-                    $list = getAccountList($_COOKIE['status']);
-                    for ($i=0; $i < count($list); $i++) 
-                    { 
-                        $flag = true;
-                        echo    "<tr align=center>
-                                    <td width=150>".$list[$i]['AccountName']."</td>
-                                </tr>";
-                    }
-                    echo    "<tr align=center>
-                                <td width=150><a href='home.php'> ADD ACCOUNT </a></td>
-                            </tr>";
-                ?>
-            </table>
+            <form method="post" action="accountCheck.php" enctype="">
+                <table width="150" border="2" align="center">
+                    <tr align="center">
+                        <td>
+                            <select name="AccountId">
+                                <?php
+                                    $list = getAccountList($_COOKIE['status']);
+                                    if(count($list) == 0)
+                                    {
+                                        echo "<option value='empty'>Create Account</option>";
+                                    }
+                                    else
+                                    {
+                                        for ($i=0; $i < count($list); $i++) 
+                                        { 
+                                            echo "<option value=".$list[$i]['AccountId'].">".$list[$i]['AccountName']."</option>";
+                                        } 
+                                    }
+                                ?>                         
+                            </select>
+                        </td>
+                    </tr>
+                    <tr align="center">
+                        <td><input type="submit" name="submit" value="OPEN"></td>
+                    </tr>
+                </table>
+            </form>
+            
         </fieldset>
     </body>
 </html>
